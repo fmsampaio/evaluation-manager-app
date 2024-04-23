48e2cf1260649b225cf431c7d243f60eac8d472f
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-04-23 11:02:46 -0300

    MainPage files

---
+++ src/Pages/MainPage.js
@@ -0,0 +1,12 @@
+import {styles} from "./MainPage.module.css"
+
+function MainPage() {
+
+    return (
+        <div>
+            <h1>Gerenciador de avaliações</h1>
+        </div>
+    )
+}
+
+export default MainPage
