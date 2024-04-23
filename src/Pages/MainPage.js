dcc30d8dfcfed909c3e701ccce2761e894ae507a
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-04-23 13:52:58 -0300

    Inclusiong of bootstrap components package and basic courses fecthing from API

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react"
 import {styles} from "./MainPage.module.css"
+import Form from 'react-bootstrap/Form'
 
 function MainPage() {
+
+    const [courses, setCourses] = useState([])
+
+    const BASE_API_URL = 'http://localhost:5000/'
+
+    useEffect( () => {
+        listCourses()
+    }, [])
+
+    function listCourses() {
+        fetch(`${BASE_API_URL}courses/`, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        })
+        .then( (resp) => (resp.json()))
+        .then( (data) => {
+            setCourses(data)           
+        })
+    }
 
     return (
         <div>
             <h1>Gerenciador de avaliações</h1>
+            <Form.Select aria-label="Curso">
+                <option>Selecione um curso</option>
+                {
+                    courses.map( (course) => (
+                        <option value={course.id}>{course.name}</option>
+                    )) 
+                }
+            </Form.Select>
         </div>
     )
 }
