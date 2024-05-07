3a314209126e22e2f1fe77526c5527af9c5064ae
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-05-07 17:11:41 -0300

    Basic router scheme for multi-page app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import EvaluationPage from './Pages/EvaluationPage';
+import EvaluationViewPage from './Pages/EvaluationViewPage';
+import NoPage from './Pages/NoPage';
+import HomePage from './Pages/HomePage';
+import Layout from './Pages/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <EvaluationPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="evaluate" element={<EvaluationPage/>} />
+          <Route path="view" element={<EvaluationViewPage/>} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/EvaluationViewPage.js
@@ -0,0 +1,9 @@
+function EvaluationViewPage() {
+    return (
+        <>
+        <h1>Visualização de avaliações</h1>
+        </>
+    )
+}
+
+export default EvaluationViewPage
