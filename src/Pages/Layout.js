474fac0591c1a6075fe028e791d8983268f22bde
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-05-10 09:02:09 -0300

    Using bootstrap navbar classes

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,29 +1,53 @@
 import { Outlet, Link } from "react-router-dom";
 import styles from "./Layout.module.css"
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Principal</Link>
-          </li>
-          <li>
-            <Link to="/evaluate">Realizar avaliação</Link>
-          </li>
-          <li>
-            <Link to="/view">Visualizar avaliações</Link>
-          </li>
-          <li>
-            <Link to="/export">Exportar avaliações</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/evaluate">Realizar avaliação</Nav.Link>
+            <Nav.Link href="/view">Visualizar avaliações</Nav.Link>
+            <Nav.Link href="/export">Exportar avaliações</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <Outlet />
     </>
   )
 };
 
 export default Layout;
+
+/*
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  }
+  
+  li {
+    float: left;
+  }
+  
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  
+  li a:hover:not(.active) {
+    background-color: #111;
+  }
+  
+  .active {
+    background-color: #04AA6D;
+  }
+  */
