de9fe49a1baa3c487892f9917024c794deba0d35
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-05-22 15:48:11 -0300

    Added modal for comments presentation at evaluation view table

--- src/Components/EvaluationViewTable.js
+++ src/Components/EvaluationViewTable.js
@@ -1,7 +1,39 @@
-import { Table } from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Table, Modal, Button } from "react-bootstrap"
 import styles from './EvaluationViewTable.module.css'
 
+function CommentsModal(props) {
+    return (
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Comentários:
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              {props.comments}
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      )
+}
+
 function EvaluationViewTable( {activity, tableInfo}  ) {
+
+    const [modalShow, setModalShow] = useState(false)
+    const [commentsAtModal, setCommentsAtModal] = useState("test")
+
+    useEffect( () => {
+    }, [commentsAtModal])
 
     function getGradesForTable(evalu) {
         if(evalu === undefined) {
@@ -36,28 +68,38 @@ function EvaluationViewTable( {activity, tableInfo}  ) {
         }
     }
 
-    function getTableHeader() {
-        return (
-            <thead className="thead-dark">
-                <tr>
-                    <th className="text-center">Nome</th>
-                    <th className="text-center">Matrícula</th>
-                    {
-                        activity.criteria.map( (crit) => (
-                            <th className="text-center">{crit.short_name} ({crit.weight*100}%)</th>
-                        ))
-                    }
-                    <th className="text-center table-success">Nota</th>
-                </tr>
-            </thead>
-        )
-    }
+    function handleModalBtnClick(e) {
+        console.log(e.target.id)
 
+        var comments = ""
+        tableInfo.forEach(info => {
+            if(info.eval) {
+                if(info.eval.id === e.target.id) {
+                    comments = info.eval.comments
+                }
+            }
+        });
+        setCommentsAtModal(comments)
+        setModalShow(true)
+    }
 
     return (
         <div className={styles.container}>
             <Table bordered striped>
-                {getTableHeader()}
+                <thead className="thead-dark">
+                    <tr>
+                        <th className="text-center">Nome</th>
+                        <th className="text-center">Matrícula</th>
+                        {
+                            activity.criteria.map( (crit) => (
+                                <th className="text-center">{crit.short_name} ({crit.weight*100}%)</th>
+                            ))
+                        }
+                        <th className="text-center table-success">Nota</th>
+                        <th className="text-center">Comentários</th>
+
+                    </tr>
+                </thead>
                 <tbody>
                 
                     {
@@ -66,11 +108,27 @@ function EvaluationViewTable( {activity, tableInfo}  ) {
                             <td>{info.student.name}</td>
                             <td>{info.student.code}</td>
                             {getGradesForTable(info.eval)}
+                            { info.eval ?
+                                <td>
+                                    {
+                                        <Button id={info.eval.id} variant="primary" size="sm" onClick={handleModalBtnClick}>
+                                            Abrir
+                                        </Button>
+                                    }
+                                </td>
+                                :
+                                <td></td>
+                            }
                         </tr>
                     ))
                     }
                 </tbody>
-            </Table>        
+            </Table>
+            <CommentsModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                comments={commentsAtModal}
+            />    
         </div>
     )
 }
